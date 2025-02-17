24997b33147b1e5e54970acc52ca8a94bc8b3ecb
BuildTools <unconfigured@null.spigotmc.org>
2025-02-17 00:40:16 -0600

    Fixed Accuracy bug
    Signed-off-by: BuildTools <unconfigured@null.spigotmc.org>

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -25,7 +25,7 @@ Moves:
 	move GUILLOTINE,   OHKO_EFFECT,                  1, NORMAL,        30,  5
 	move RAZOR_WIND,   NO_ADDITIONAL_EFFECT,        80, NORMAL,        75, 10
 	move SWORDS_DANCE, ATTACK_UP2_EFFECT,            0, NORMAL,       100, 30
-	move CUT,          NO_ADDITIONAL_EFFECT,        50, NORMAL,        95, 30
+	move CUT,          NO_ADDITIONAL_EFFECT,        50, NORMAL,       100, 30
 	move GUST,         NO_ADDITIONAL_EFFECT,        40, FLYING,       100, 35
 	move WING_ATTACK,  NO_ADDITIONAL_EFFECT,        60, FLYING,       100, 35
 	move TWISTER,      FLINCH_SIDE_EFFECT2,         40, DRAGON,       100, 20
